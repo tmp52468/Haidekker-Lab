aa8a61fcca2d2b1e2339022f9c19f51a0d61d90f
Tony Pham <70601000+tmp52468@users.noreply.github.com>
2020-11-23 20:08:37 -0500

    Updated script to be more user friendly

--- auto_image_rclone.sh
+++ auto_image_rclone.sh
@@ -4,18 +4,18 @@
 DATE=$(date +%F@%H:%M:%S)
 
 # load in the files from the cloud server
-rclone sync -v onedriveTest:"CF Imaging RPi" /home/pi/rclone/
+rclone sync -v onedrive:PathToOneDriveFolder /home/pi/rclone/
 
 # run the imaging script
-python /home/pi/MIPI_Camera/RPI/python/rclone-camera-test.py
+python /home/pi/MIPI_Camera/RPI/python/cf_imaging.py
 
 # process the image (skipped for now)
 
 
 # move the processed (unprocessed) image to the synced folder
 echo $DATE
-mv /home/pi/rclone-test.jpg /home/pi/rclone/$DATE.jpg
+mv /home/pi/cf_image.jpg /home/pi/rclone/$DATE.jpg
 
 # upload the files back to the cloud server
-rclone sync -v /home/pi/rclone/ onedriveTest:"CF Imaging RPi"
+rclone sync -v /home/pi/rclone/ onedriveTest:NameOfOneDriveFolder
 
