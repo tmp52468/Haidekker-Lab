ef3f5b7ea3e6115ba2c9d648745902466597ddd2
Tony Pham <70601000+tmp52468@users.noreply.github.com>
2020-11-24 22:08:03 -0500

    Update auto_image_rclone.sh

--- auto_image_rclone.sh
+++ auto_image_rclone.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# variables
+RCLONE="/home/pi/rclone" # Rclone directory for cloud sync
+ONEDRIVE="onedrive:CF_Imaging_RPi" # OneDrive directory
+SAMBA="/home/pi/sambaShare" # SambaShare directory
+TYPE="multiple" # Type of analysis (single or multiple)
+
 # set current date
 DATE=$(date +%F@%H:%M:%S)
 
 # load in the files from the cloud server
-rclone sync -v onedrive:PathToOneDriveFolder /home/pi/rclone/
+rclone sync -v $ONEDRIVE $RCLONE/
 
 # run the imaging script
 python /home/pi/MIPI_Camera/RPI/python/cf_capture.py
 
-# process the image (skipped for now)
+# process the image
+imagej -b $SAMBA/CF_Imaging.ijm "/home/pi/cf_image.jpg $SAMBA/$DA$
+imagej -b $SAMBA/CF_Imaging.ijm "/home/pi/cf_image.jpg $RCLONE/$D$
 
-
-# move the processed (unprocessed) image to the synced folder
-echo $DATE
-mv /home/pi/cf_image.jpg /home/pi/rclone/$DATE.jpg
+# move the processed (and unprocessed) image(s) to the synced fol$
+cp /home/pi/cf_image.jpg $RCLONE/$DATE.jpg
+mv /home/pi/cf_image.jpg $SAMBA/$DATE.jpg
 
 # upload the files back to the cloud server
-rclone sync -v /home/pi/rclone/ onedriveTest:NameOfOneDriveFolder
-
+rclone sync -v $RCLONE/ $ONEDRIVE
