0ae564baa97ab1cb4fa67ac8e6eb156ffaaed96d
Tony Pham <70601000+tmp52468@users.noreply.github.com>
2020-11-30 16:42:52 -0500

    Update auto_cf_imaging.sh

--- auto_cf_imaging.sh
+++ auto_cf_imaging.sh
@@ -2,11 +2,12 @@
 
 # variables
 RCLONE="/home/rclone" # Rclone directory for cloud sync
-ONEDRIVE="onedrive:CF_Imaging_RPi" # OneDrive directory on cloud drive
-SAMBA="/home/smbshare" # SambaShare/CIFS directory
+ONEDRIVE="onedrive:<path/to/directory>" # OneDrive directory on cloud drive
+SAMBA="/home/smbshare/<subdirectory>" # SambaShare/CIFS directory
 TYPE="single" # Type of analysis (single or multiple)
 
 # set current date
+NAME=<nameofsystem>
 DATE=$(date +%F@%H:%M:%S)
 
 # load in the files from the cloud server
@@ -16,12 +17,12 @@ rclone sync -v $ONEDRIVE $RCLONE/
 python /home/pi/MIPI_Camera/RPI/python/cf_capture.py
 
 # process the image
-imagej -b $SAMBA/CF_Imaging.ijm "/home/pi/cf_image.jpg $SAMBA/$DATE $TYPE"
-imagej -b $SAMBA/CF_Imaging.ijm "/home/pi/cf_image.jpg $RCLONE/$DATE $TYPE"
+imagej -b /home/pi/CF_Imaging.ijm "/home/pi/cf_image.jpg $SAMBA/$NAME_$DATE $TYPE"
+imagej -b /home/pi/CF_Imaging.ijm "/home/pi/cf_image.jpg $RCLONE/$NAME_$DATE $TYPE"
 
 # move the processed (and unprocessed) image(s) to the synced fol$
-cp /home/pi/cf_image.jpg $RCLONE/$DATE.jpg
-mv /home/pi/cf_image.jpg $SAMBA/$DATE.jpg
+cp /home/pi/cf_image.jpg $SAMBA/$NAME_$DATE.jpg
+mv /home/pi/cf_image.jpg $RCLONE/$NAME_$DATE.jpg
 
 # upload the files back to the cloud server
 rclone sync -v $RCLONE/ $ONEDRIVE
